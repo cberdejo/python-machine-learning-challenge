697b6cfb7a656c542ccf18e410f6155b71fe6f8d
cberdejo <cberdejo@uma.es>
2025-04-23 16:22:11 +0200

    feat: added logic to predict data given a model

---
+++ backend-python-challenge/src/api/controllers/predict_controller.py
@@ -0,0 +1,60 @@
+from typing import List
+from api.models.animal_data import AnimalData
+from api.models.generic_response import GenericResponse
+from api.models.synthetic_data import SyntheticDataParams
+from api.utils import (
+    get_model_deserialized_from_minio,
+)
+from config.minio_config import BUCKET_DATA, BUCKET_MODELS
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from machine_learning.predict import predict
+from machine_learning.models.analysis_result import AnalysisResult
+from minio import Minio
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+async def predict_controller(
+    request: Request, model: SyntheticDataParams, animal_data: List[AnimalData]
+):
+    response = GenericResponse(code=500, message="Something went wrong", data=None)
+
+    minio_client: Minio = request.app.state.minio_client
+    object_path = f"{model.seed}-{model.number_of_datapoints}/model.pkl"
+
+    # Try to get the model from MinIO
+
+    try:
+        model, label_encoder = get_model_deserialized_from_minio(
+            minio_client, BUCKET_MODELS, object_path
+        )
+    except FileNotFoundError:
+        logger.warning("Model not found in MinIO")
+        response.code = 404
+        response.message = f"Model not found in MinIO, use train endpoint to train a model: {os.getenv('HOST', '0.0.0.0')}:8000/api/v1/train?seed={model.seed}&number_of_datapoints={model.number_of_datapoints}"
+        response.data = None
+        return JSONResponse(status_code=response.code, content=response.model_dump())
+    except Exception as e:
+        logger.error(f"Error fetching model from MinIO: {str(e)}")
+        response.code = 500
+        response.message = f"Error validating model: {str(e)}"
+        return JSONResponse(status_code=response.code, content=response.model_dump())
+
+    # predict
+    try:
+        result: List[AnimalData] = predict(
+            animal_data, model, label_encoder=label_encoder
+        )
+        response.data = result
+        response.message = "Model validated successfully."
+        response.code = 200
+        response.data = result
+        return JSONResponse(status_code=response.code, content=response.model_dump())
+    except Exception as e:
+        logger.error(f"Error validating model: {str(e)}")
+        response.code = 500
+        response.message = f"Error validating model: {str(e)}"
+        return JSONResponse(status_code=response.code, content=response.model_dump())
---
+++ backend-python-challenge/src/machine_learning/predict.py
@@ -0,0 +1,46 @@
+from typing import List
+from api.models.animal_data import AnimalData
+from .machine_learning_functions import (
+    prepare_data_for_machine_learning,
+    prepare_data_for_prediction,
+    scale_data,
+)
+from sklearn.preprocessing import LabelEncoder
+import polars as ps
+
+
+def predict(
+    data: List[AnimalData], model, label_encoder: LabelEncoder
+) -> List[AnimalData]:
+    """
+    Predict the labels of the given animal data using the provided model.
+    Args:
+        data (List[AnimalData]): List of AnimalData objects to predict.
+        model: Trained machine learning model.
+        label_encoder (LabelEncoder): Label encoder for decoding labels.
+    Returns:
+        List[AnimalData]: List of AnimalData objects with predicted labels.
+    """
+    # Transform data to DataFrame
+    df = ps.DataFrame(
+        [
+            {key: value for key, value in animal.dict().items() if key != "label"}
+            for animal in data
+        ]
+    )
+
+    # Prepare features for prediction
+    X = prepare_data_for_prediction(df)
+
+    # Scale data
+    X_scaled = scale_data(X)
+
+    # Predict using the model
+    y_pred = model.predict(X_scaled)
+    labels = label_encoder.inverse_transform(y_pred)
+
+    # Add labels to the original data
+    for animal, label in zip(data, labels):
+        animal.label = label
+
+    return data
