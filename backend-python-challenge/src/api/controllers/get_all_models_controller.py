8aabbb08eab1ac4d09f531bacce4dc65632af054
cberdejo <cberdejo@uma.es>
2025-04-23 16:21:50 +0200

    feat: added controller to get all models stored in Minio

---
+++ backend-python-challenge/src/api/controllers/get_all_models_controller.py
@@ -0,0 +1,34 @@
+from api.models.generic_response import GenericResponse
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from minio import Minio
+
+
+async def get_all_models(request: Request) -> JSONResponse:
+    """
+    Get all models from the database.
+    Args:
+        request (Request): The FastAPI request object.
+    Returns:
+    """
+    response: GenericResponse = GenericResponse(
+        code=500, message="Something went wrong", data=None
+    )
+
+    minio_client: Minio = request.app.state.minio_client
+
+    # Get the list of models from MinIO
+    try:
+        models = minio_client.list_objects(
+            bucket_name="models",
+            prefix="",
+            recursive=False,
+        )
+        response.data = [model.object_name for model in models]
+        response.code = 200
+        response.message = "Models fetched successfully."
+        return JSONResponse(status_code=response.code, content=response.model_dump())
+    except Exception as e:
+        response.code = 500
+        response.message = f"Error fetching models from MinIO: {str(e)}"
+        return JSONResponse(status_code=response.code, content=response.model_dump())
